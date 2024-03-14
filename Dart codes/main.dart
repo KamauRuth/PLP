50d949d837f8949b15ba3a314c5cf23c4367fc1a
KamauRuth <101416148+KamauRuth@users.noreply.github.com>
2024-03-15 01:23:24 +0300

    Add files via upload

---
+++ Dart codes/main.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  print("Hello world");
+  String name= "Ruth";
+  num age = 20;
+  bool isMarried = false;
+   print (name);
+   print ("my age is $age and my status is $isMarried");
+
+}
